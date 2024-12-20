3a776a90531547087dae570bca5ca903768c336f
NureTymofiienkoSnizhana <snizhana.tymofiienko@nure.ua>
2024-12-20 13:26:12 +0200

    commit

--- main.go
+++ main.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/NureTymofiienkoSnizhana/arkpz-pzpi-22-9-tymofiienko-snizhana/Pract1/arkpz-pzpi-22-9-tymofiienko-snizhana-task2/src/api"
 	"github.com/NureTymofiienkoSnizhana/arkpz-pzpi-22-9-tymofiienko-snizhana/Pract1/arkpz-pzpi-22-9-tymofiienko-snizhana-task2/src/data/mongodb"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"os"
 )
 
 func main() {
 	ctx := context.Background()
 
-	clientOptions := options.Client().SetAuth(options.Credential{
-		Username: "welnersis",
-		Password: "B4H*$Xt@TUyRf9$",
-	}).ApplyURI("mongodb+srv://petandhealth.dtpxu.mongodb.net/?retryWrites=true&w=majority&appName=PetAndHealth")
+	username := os.Getenv("MONGO_USERNAME")
+	password := os.Getenv("MONGO_PASSWORD")
+	if username == "" || password == "" {
+		panic("MONGO_USERNAME або MONGO_PASSWORD не встановлені")
+	}
+
+	mongoURI := fmt.Sprintf("mongodb+srv://%s:%s@petandhealth.dtpxu.mongodb.net/?retryWrites=true&w=majority&appName=PetAndHealth", username, password)
+	clientOptions := options.Client().ApplyURI(mongoURI)
+
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		panic(err)
--- src/api/router.go
+++ src/api/router.go
@@ -3,12 +3,12 @@ package api
 import (
 	"context"
 	"github.com/NureTymofiienkoSnizhana/arkpz-pzpi-22-9-tymofiienko-snizhana/Pract1/arkpz-pzpi-22-9-tymofiienko-snizhana-task2/src/api/handlers"
+	"github.com/NureTymofiienkoSnizhana/arkpz-pzpi-22-9-tymofiienko-snizhana/Pract1/arkpz-pzpi-22-9-tymofiienko-snizhana-task2/src/data"
 	"github.com/NureTymofiienkoSnizhana/arkpz-pzpi-22-9-tymofiienko-snizhana/Pract1/arkpz-pzpi-22-9-tymofiienko-snizhana-task2/src/middle"
 	"github.com/go-chi/chi/v5"
-
-	"github.com/NureTymofiienkoSnizhana/arkpz-pzpi-22-9-tymofiienko-snizhana/Pract1/arkpz-pzpi-22-9-tymofiienko-snizhana-task2/src/data"
 	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -19,6 +19,7 @@ type Config struct {
 func Run(config Config) {
 	r := chi.NewRouter()
 
+	// Middleware
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
@@ -29,18 +30,16 @@ func Run(config Config) {
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
 	})
-
 	r.Use(middleware.Timeout(60 * time.Second))
 
+	// Routes
 	r.Route("/api/pet-and-health", func(r chi.Router) {
 		r.Route("/v1/login", func(r chi.Router) {
-			//r.Use(middle.AuthMiddleware)
 			r.Get("/auth", handlers.Auth)
 			r.Post("/registration", handlers.Registration)
 			r.Post("/create-admin", handlers.CreateAdmin)
 		})
 		r.Route("/v1/admin/pets", func(r chi.Router) {
-			//r.Use(middle.AuthMiddleware)
 			r.Use(middle.MockUser("user"))
 			r.Use(middle.CheckRole("admin"))
 			r.Post("/add-pet", handlers.AddPet)
@@ -48,49 +47,14 @@ func Run(config Config) {
 			r.Get("/get-pets", handlers.GetPets)
 			r.Put("/update-pet", handlers.UpdatePet)
 		})
-		r.Route("/v1/admin/devices", func(r chi.Router) {
-			//r.Use(middle.AuthMiddleware)
-			r.Use(middle.MockUser("admin"))
-			r.Use(middle.CheckRole("admin"))
-			r.Post("/add-device", handlers.AddDevice)
-			r.Put("/update-device", handlers.UpdateDevice)
-		})
-		r.Route("/v1/admin/users", func(r chi.Router) {
-			//r.Use(middle.AuthMiddleware)
-			r.Use(middle.MockUser("admin"))
-			r.Use(middle.CheckRole("admin"))
-			r.Put("/update-user", handlers.UpdateUser)
-			r.Delete("/delete-user", handlers.DeleteUser)
-		})
-		r.Route("/v1/user:id", func(r chi.Router) {
-			//r.Use(middle.AuthMiddleware)
-			r.Put("/update-user", handlers.UpdateUser)
-			r.Get("/profile", handlers.UserInfo)
-			//r.Post("/logout", handlers.Logout)
-		})
-		r.Route("/v1/user", func(r chi.Router) {
-			//r.Use(middle.AuthMiddleware)
-			r.Get("/pet-info", handlers.PetInfo)
-		})
-		r.Route("/v1/owner", func(r chi.Router) {
-			//r.Use(middle.AuthMiddleware)
-			r.Use(middle.MockUser("user"))
-			r.Use(middle.CheckRole("user"))
-			r.Get("/owner-pets", handlers.GetOwnerPets)
-		})
-		r.Route("/v1/vet", func(r chi.Router) {
-			//r.Use(middle.AuthMiddleware)
-			r.Use(middle.MockUser("user"))
-			r.Use(middle.CheckRole("vet"))
-			r.Get("/pet-report/pdf", handlers.GetPetReport)
-			r.Get("/get-pets", handlers.GetPets)
-		})
-		r.Route("/v1/health-data", func(r chi.Router) {
-			r.Post("/device", handlers.AddHealthData)
-		})
 	})
 
-	err := http.ListenAndServe(":8080", r)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	err := http.ListenAndServe(":"+port, r)
 	if err != nil {
 		panic(err)
 	}
